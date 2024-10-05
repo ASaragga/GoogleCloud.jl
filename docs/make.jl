0146c72498fa8b40e3d2d3b39afcd29d38ef1cc8
Antonio Saragga Seabra <47437960+ASaragga@users.noreply.github.com>
2024-10-05 04:42:25 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,6 @@ makedocs(; doctest=true, sitename="Google Cloud JSON APIs")
 
 deploydocs(;
     deps=Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo="github.com/JuliaCloud/GoogleCloud.jl.git",
+    repo="github.com/ASaragga/GoogleCloud.jl",
     push_preview=true,
 )
